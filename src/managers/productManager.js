0f9c4e94a839782edf2010b65ffbbf9f8227b792
tomasjrl <tstabilini@gmail.com>
2024-09-13 23:47:52 -0300

    creado codigo para getAllProducts y getProduct + simulando una lista de objetos a modo temporal en products.json

--- src/managers/productManager.js
+++ src/managers/productManager.js
@@ -1,9 +1,40 @@
-export function getAllProducts(req, res) {
-  res.send('Obteniendo todos los productos');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dataPath = path.join(__dirname, '..', '..', 'data', 'products.json');
+
+
+
+export function getAllProducts(res) {
+  try {
+    const products = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+    res.json(products);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      res.status(404).json({ message: 'Archivo de productos no encontrado' });
+    } else {
+      res.status(500).json({ message: 'Error interno al obtener productos' });
+    }
+  }
 }
 
 export function getProduct(req, res) {
-  res.send(`Obteniendo el producto con id ${req.params.pid}`);
+  try {
+    const products = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+    const productId = req.params.pid;
+    const product = products.find((product) => product.id === parseInt(productId));
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: 'Producto no encontrado' }); 
+    }
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      res.status(500).json({ message: 'Error interno al obtener productos' });
+    }
+  }
 }
 
 export function createProduct(req, res) {
