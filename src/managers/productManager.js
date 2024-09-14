6f1e20837ee005ea5e0634d5767619ca27f6403c
tomasjrl <tstabilini@gmail.com>
2024-09-14 00:32:32 -0300

    agrego codigo para updateProduct

--- src/managers/productManager.js
+++ src/managers/productManager.js
@@ -5,8 +5,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const dataPath = path.join(__dirname, '..', '..', 'data', 'products.json');
 
-
-
 export function getAllProducts(req,res) {
   try {
     const products = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
@@ -37,10 +35,6 @@ export function getProduct(req, res) {
   }
 }
 
-// export function createProduct(req, res) {
-//   res.send('Creando un nuevo producto');
-// }
-
 export function createProduct(req, res) {
   try {
     const products = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
@@ -69,7 +63,27 @@ export function createProduct(req, res) {
 }
 
 export function updateProduct(req, res) {
-  res.send(`Actualizando el producto con id ${req.params.pid}`);
+  try {
+    const products = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+    const productId = parseInt(req.params.pid);
+    const productIndex = products.findIndex(p => p.id === productId);
+
+    if (productIndex !== -1) {
+      const updatedProduct = {
+        ...products[productIndex],
+        ...req.body,
+      };
+
+      products[productIndex] = updatedProduct;
+      fs.writeFileSync(dataPath, JSON.stringify(products, null, 2));
+
+      res.json(updatedProduct);
+    } else {
+      res.status().json({ message: 'Producto no encontrado' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error interno al actualizar el producto' });
+  }
 }
 
 export function deleteProduct(req, res) {
