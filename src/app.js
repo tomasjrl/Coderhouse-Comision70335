d31e8afddc73ba17c3cd35ebfc758db08de9ba1a
tomasjrl <tstabilini@gmail.com>
2024-09-16 20:36:40 -0300

    incorporando handlebars (por ahora con datos estaticos)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import hbs from 'express-handlebars';
+import handlebars from 'express-handlebars';
+import __dirname from "./utils.js";
 import 'dotenv/config';
 import cartRouter from './routes/cartRouter.js';
 import productRouter from './routes/productRouter.js';
@@ -9,31 +10,27 @@ const app = express();
 const api = process.env.API_URL;
 const PORT = process.env.PORT || 8080;
 
-// revisar no funciona
-app.engine('handlebars', hbs.engine({
-  defaultLayout: 'main',
-  layoutsDir: '../views/layouts',
-  partialsDir: '../views/partials'
-}));
+app.engine("handlebars", handlebars.engine());
 
-// revisar no funciona
-app.set('view engine', 'handlebars');
-app.set('views', '../view'); // revisar ruta no funciona
+app.set("views", __dirname + "/views");
+
+app.set("view engine", "handlebars");
+
+app.use(express.static(__dirname + "/public"));
 
 app.use(express.json());
 
-// falta configurar
-// app.get('/', (req, res) => { 
-//   const products = getAllProducts();
-//   res.render('index', { products });
+// AL MOMENTO NO FUNCIONA, FALTA COMPLETAR CODIGO
+//  app.get('/', (req, res) => { 
+//  const products = getAllProducts();
+// res.render('index', { products });
 // });
-
-// falta configurar
-// app.get('/productos-en-tiempo-real', (req, res) => {
+// AL MOMENTO NO FUNCIONA, FALTA COMPLETAR CODIGO
+//  app.get('/productos-en-tiempo-real', (req, res) => {
 //   res.render('realTimeProducts', { products: [] }); // por ahora, pasamos una lista vacía
-// });
+//  });
 
-// probando estaticamente lista de productos 
+// probando estaticamente lista de productos = FUNCIONA
 app.get('/', (req, res) => {
   const productos = [
     { id: 1, nombre: 'Producto 1', precio: 10.99 },
@@ -43,12 +40,12 @@ app.get('/', (req, res) => {
   res.render('index', { products: productos });
 });
 
-// probando estaticamente lista de productos
+// probando estaticamente lista de productos  = FUNCIONA
 app.get('/productos-en-tiempo-real', (req, res) => {
   const productos = [
-    { id: 1, nombre: 'Producto 1', precio: 10.99 },
-    { id: 2, nombre: 'Producto 2', precio: 9.99 },
-    { id: 3, nombre: 'Producto 3', precio: 12.99 },
+    { id: 1, nombre: 'Producto 10', precio: 10.99 },
+    { id: 2, nombre: 'Producto 11', precio: 9.99 },
+    { id: 3, nombre: 'Producto 12', precio: 12.99 },
   ];
   res.render('realTimeProducts', { products: productos });
 });
