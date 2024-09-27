a327b98590eb230c1e69867ec4c8f727803a25c3
tomasjrl <tstabilini@gmail.com>
2024-09-27 18:47:52 -0300

    agrego page a products

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10,6 +10,16 @@ const helpers = {
   },
   subtract: function(v1, v2) {
     return parseInt(v1) - parseInt(v2);
+  },
+  forPages: function(n, options) {
+    let pages = '';
+    for (let i = 1; i <= n; i++) {
+      pages += `<option value="${i}" ${i === options.data.root.page ? 'selected' : ''}>${i} de ${n}</option>`;
+    }
+    return pages;
+  },
+  isSelected: function(value, currentPage) {
+    return (value == currentPage) ? 'selected' : '';
   }
 };
 
